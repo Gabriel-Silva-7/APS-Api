e2f7e41d0b22900091399b6e9c9e4e1af07386a2
Gabriel Silva <66033771+Gabriel-Silva-7@users.noreply.github.com>
2023-12-05 01:59:33 -0300

    hotfix

--- src/controllers/getSell.js
+++ src/controllers/getSell.js
@@ -2,10 +2,12 @@ import getSell from "../services/getSells.js";
 
 const getSellsFromDb = async (req, res) => {
   try {
-    const response = await getSell();
+    const days = req.body.days;
+    console.log(days);
+    const response = await getSell(days);
     res.status(200).send(response);
   } catch (error) {
-    res.status(500).send("failed to register new user", error);
+    res.status(500).send("Failed to get sells", error);
   }
 };
 
--- src/services/getSells.js
+++ src/services/getSells.js
@@ -1,13 +1,13 @@
 import azureDB from "../connections/azure.js";
 
-const getUsers = async () => {
+const getUsers = async (days) => {
   try {
     const response = await azureDB.query(
-      `SELECT SUM(Valor) as ValorTotalDia FROM Vendas GROUP BY Data_Venda ORDER BY Data_Venda`
+      `SELECT SUM(Valor) as ValorTotalDia, Data_Venda FROM Vendas WHERE Data_Venda >= DATEADD(day, ${days}, GETDATE()) GROUP BY Data_Venda ORDER BY Data_Venda `
     );
     return response[0];
   } catch (e) {
-    throw new Error("Failed to get users");
+    throw new Error("Failed to get sells", e);
   }
 };
 
